e5e059552d3dba37ad0de1fe46b66af063b80fc1
cronugs <cronugs@outlook.com>
2022-08-09 22:39:40 +1000

    results now show a thumbnail of the character

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import mitt from 'mitt';
 import "./assets/main.css";
 
+const emitter = mitt();
 const app = createApp(App);
 
 app.use(router);
-
+app.config.globalProperties.emitter = emitter;
 app.mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js"
