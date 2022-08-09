5df3774707431b5dad38d80b22ec2fc6c515fe26
cronugs <cronugs@outlook.com>
2022-08-09 10:53:02 +1000

    first commit with Vue, Typescript and Bootstrap setup

---
+++ src/components/YourChampions.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+  characterData;
+}>();
+</script>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  mounted() {
+    console.log(this.characterData);
+  },
+});
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
